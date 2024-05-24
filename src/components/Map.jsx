1c30ced8a44ba3f2210eca7969423f7c9b8353fd
MarcusVinici271 <131982502+MarcusVinici271@users.noreply.github.com>
2024-05-24 23:26:23 +0000

    teste

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,34 @@
-import './Map.css';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+// import './Map.css';
+// import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 
-//AIzaSyD46BVRzxRTFYbAO3BSzKCpXcmR28BIAmE
+// //AIzaSyD46BVRzxRTFYbAO3BSzKCpXcmR28BIAmE
 
-export default function Map(){
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyD46BVRzxRTFYbAO3BSzKCpXcmR28BIAmE"
-  })
+// export default function Map(){
+//   const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: "AIzaSyD46BVRzxRTFYbAO3BSzKCpXcmR28BIAmE"
+//   })
 
-  const position = {
-    lat: -22.8899167,
-    lng: -43.1218042
-  }
+//   const position = {
+//     lat: -22.8899167,
+//     lng: -43.1218042
+//   }
   
-  return(
-    <div className='divmap'>
-    <h2>Endereço</h2> 
-    <div className='map'>    
+//   return(
+//     <div className='divmap'>
+//     <h2>Endereço</h2> 
+//     <div className='map'>    
          
-      {isLoaded ? (
-          <GoogleMap
-            mapContainerStyle={{width: '100%', height: '100%'}}
-            center={position}
-            zoom={15}
-            Marker={position}
-            ></GoogleMap>          
-      ) : <></>}
-    </div>
-      </div>
-  )
-}
+//       {isLoaded ? (
+//           <GoogleMap
+//             mapContainerStyle={{width: '100%', height: '100%'}}
+//             center={position}
+//             zoom={15}
+//             Marker={position}
+//             ></GoogleMap>          
+//       ) : <></>}
+//     </div>
+//       </div>
+//   )
+// }
